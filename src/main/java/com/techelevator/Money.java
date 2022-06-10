3d663a395d0dcf6e5a9127b8ed652ee4a451a4d9
David Tran <dall.tran@gmail.com>
2022-06-10 14:01:10 -0400

    Updated main menu items

--- src/main/java/com/techelevator/Money.java
+++ src/main/java/com/techelevator/Money.java
@@ -3,11 +3,17 @@ package com.techelevator;
 import java.math.BigDecimal;
 import java.util.Scanner;
 
+import static com.techelevator.Program.mainMenu;
+
 public class Money {
     private BigDecimal amountOwed;
     private BigDecimal amountPaid;
     private BigDecimal changeDue;
 
+
+//    public Money(VendingMachine machine) {
+//            this.machine = machine;
+//    }
 
     public BigDecimal getAmountOwed() {
         return amountOwed;
@@ -16,8 +22,6 @@ public class Money {
     public BigDecimal getAmountPaid() {
         return amountPaid;
     }
-
-
 
     public void setAmountOwed(BigDecimal amountOwed) {
         this.amountOwed = amountOwed;
@@ -30,13 +34,33 @@ public class Money {
         changeDue= amountPaid.subtract(amountOwed);
         return changeDue;
     }
-    public void transaction(){
-        Scanner transaction = new Scanner(System.in);
 
-        System.out.println("(1) Feed Money\n(2) Select Product\n(3) Finish Transaction");
-        String selection = transaction.nextLine();
-        if("1"){
-
-        }
-    }
+//    public void purchaseMenu() {
+//        Money money = new Money(machine);
+//
+//        Scanner purchaseMenu = new Scanner(System.in);
+//        // VendingMachine machine;
+//        System.out.println("(1) Feed Money\n(2) Select Product\n(3) Finish Transaction");
+//        String selection = purchaseMenu.nextLine();
+//
+//
+//        System.out.println("Please enter your selection eg. 'A3'");
+//        String userSelection = purchaseMenu.nextLine().toUpperCase();
+//
+//        //TODO have to hit enter twice. need to fix
+//        Product userProductChoice = machine.getSlotProducts().get(userSelection); //getting value from slot products
+//
+//        //for (String slotProduct: machine.getSlotProducts().keySet()){
+//
+//        if (userProductChoice.getProductQuantity() == 0) {
+//            System.out.println("Sorry, this item is sold out.");
+//            mainMenu();
+//        } else if (!machine.getSlotProducts().containsKey(userSelection)) {
+//            System.out.println("Invalid selection. Have fun going back to main menu.");
+//            mainMenu();
+//        } else {
+//            userProductChoice.setProductQuantity(userProductChoice.getProductQuantity() - 1);
+//        }
+//    }
 }
+
--- src/main/java/com/techelevator/Program.java
+++ src/main/java/com/techelevator/Program.java
@@ -2,62 +2,92 @@ package com.techelevator;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-
 		mainMenu();
 	}
 
 	public static void mainMenu() {
-		System.out.println("(1) Display Vending Machine Items\n(2) Purchase\n(3) Exit");
 		Scanner scanner = new Scanner(System.in);
 		VendingMachine machine = new VendingMachine();
-		String userInput = scanner.nextLine();
+		String userInput;
 
-		if (userInput.equals("1")) {
-			String filePath = "C:/Users/Student/workspace/java-mini-capstone-module-1-team-2/vendingmachine.csv";
-			File inventory = new File(filePath);
-			try (Scanner scan = new Scanner(inventory)) {
-				while (scan.hasNextLine()) {
-					String scanLine = scan.nextLine() + ", ";
-					int quantity = machine.getSlotProducts().get(scanLine.substring(0, 2)).getProductQuantity();
-					System.out.println(scanLine + "Quantity: " + quantity);
+		// insert loop here
+		do {
+			System.out.println("(1) Display Vending Machine Items\n(2) Purchase\n(3) Exit");
+			userInput = scanner.nextLine();
+
+			if (userInput.equals("1")) {
+				String filePath = "C:/Users/Student/workspace/java-mini-capstone-module-1-team-2/vendingmachine.csv";
+				File inventory = new File(filePath);
+				try (Scanner scan = new Scanner(inventory)) {
+					while (scan.hasNextLine()) {
+						String scanLine = scan.nextLine() + ", ";
+						int quantity = machine.getSlotProducts().get(scanLine.substring(0, 2)).getProductQuantity();
+						System.out.println(scanLine + "Quantity: " + quantity);
+					}
+				} catch (FileNotFoundException e) {
+					System.out.println("File not found.");
 				}
+			} else if (userInput.equals("2")) {        // Selecting PURCHASE
 
-			} catch (FileNotFoundException e) {
-				System.out.println("File not found.");
-			}
-			// create new method for purchase & feed money
-			System.out.println("Please enter your selection eg. 'A3'");
-			String userSelection = scanner.nextLine().toUpperCase();
+				System.out.println("(1) Feed Money\n(2) Select Product\n(3) Finish Transaction");
+				String selection = scanner.nextLine();
 
-			//TODO have to hit enter twice. need to fix
-			Product userProductChoice = machine.getSlotProducts().get(userSelection); //getting value from slot products
+				BigDecimal moneyProvided = new BigDecimal(0.00);
 
-			//for (String slotProduct: machine.getSlotProducts().keySet()){
+					if (selection.equals("1")) {
+						do {
+							System.out.println("How much money do you want to insert?");
+							System.out.println("(1) $1.00\n(2) $5.00\n(3) $10.00\n(4) $20.00\n(5) Stop adding money");
+							selection = scanner.nextLine();
+									if (selection.equals("1")){		// TODO fix addition to moneyProvided
+										moneyProvided.add(new BigDecimal("1.00"));
+									}
+									else if (selection.equals("2")){
+										moneyProvided.add(new BigDecimal("5.00"));
+									}
+									else if (selection.equals("3")){
+										moneyProvided.add(new BigDecimal("10.00"));
+									}
+									else if (selection.equals("4")){
+										moneyProvided.add(new BigDecimal("20.00"));
+									}
+									else {
+										System.out.println("Invalid input");
+									}
+								System.out.println("Current Money Provided: " + moneyProvided);
 
-			if (userProductChoice.getProductQuantity() == 0) {
-				System.out.println("Sorry, this item is sold out.");
-				mainMenu();
-			}
-				else if(!machine.getSlotProducts().containsKey(userSelection)) {
-				System.out.println("Invalid selection. Have fun going back to main menu.");
-				mainMenu();
-			}
-				else {
-					userProductChoice.setProductQuantity(userProductChoice.getProductQuantity()-1);
+						} while (!selection.equals("5"));
+					}
+
+
+				System.out.println("Please enter your selection eg. 'A3'");
+				String userSelection = scanner.nextLine().toUpperCase();
+
+				//TODO have to hit enter twice. need to fix
+				Product userProductChoice = machine.getSlotProducts().get(userSelection); //getting value from slot products
+
+				//for (String slotProduct: machine.getSlotProducts().keySet()){
+
+				if (userProductChoice.getProductQuantity() == 0) {
+					System.out.println("Sorry, this item is sold out.");
+					mainMenu();
+				} else if (!machine.getSlotProducts().containsKey(userSelection)) {
+					System.out.println("Invalid selection. Have fun going back to main menu.");
+					mainMenu();
+				} else {
+					userProductChoice.setProductQuantity(userProductChoice.getProductQuantity() - 1);
 				}
-
 			}
 
+			System.out.println();
 
-			}
+		} while (!userInput.equals("3"));
 
-
-			}
-
-
+	}
+}
